497a770456e2eb9c43b926435867d04c3962f113
rowl <airowl.gasga@gmail.com>
2022-02-24 20:09:30 +0100

    inserito condizioni

--- js/script.js
+++ js/script.js
@@ -21,18 +21,43 @@ btn.addEventListener('click', function(){
     // * creiamo un ciclo For per stampare i numeri
     for (let i = 0; i <= 100; i++){
 
-    // * stampare i numeri da 1 a 100
-    // console.log(i);
+        // * stampare i numeri da 1 a 100
+        // console.log(i);
 
-    // * condizione per verificare se i è multiplo di 3 e se i è multiplo di 5
-    if (i % 3 == 0) {
-        console.log(i + ' Fizz' + ' questo è un multiplo di 3');
-    } else if (i % 5 == 0) {
-        console.log(i + ' Buzz' + ' questo è un multiplo di 5');
-    } else{
-        console.log(i + ' FizzBuzz' + ' questo non è multiplo di 3 e 5');
-    }
+        // * condizione per verificare se i è multiplo di 3 e se i è multiplo di 5
+        if ( (i % 3 == 0) && (i % 5 == 0) ) { 
+
+            console.log(i + ' FizzBuzz' + ' questo è un multiplo di 3 e 5!');
+
+            // ? ho creato l'elemento tag LI per ogni numbero multipli di 3 e 5
+            const newLi = document.createElement('li');
+            newLi.innerText = i;
+
+            document.getElementById('my-list').append(newLi);
+            
+
+        } else if (i % 3 == 0) { // ! se i diviso 3 dà resto 0 è multiplo di 3
+
+            console.log(i + ' Fizz' + ' questo è un multiplo di 3');
+
+        } else if (i % 5 == 0) {
+
+            console.log(i + ' Buzz' + ' questo è un multiplo di 5');
+
+        } else {
+
+            console.log(i + ' questo NON è multiplo di 3 e 5');
+
+        }
+
+        // if ( (i % 3 == 0) && (i % 5 == 0) ) {
+
+        //     console.log(i + ' FizzBuzz' + ' questo è un multiplo di 3 e 5!');
+    
+        // } 
 
     }
 });
+
+
 
