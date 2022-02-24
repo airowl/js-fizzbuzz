0a64f89f08a2902a1b71034e6a7499208d131e37
rowl <airowl.gasga@gmail.com>
2022-02-24 20:27:47 +0100

    creato tag li per ogni tipo di number

--- js/script.js
+++ js/script.js
@@ -21,32 +21,60 @@ btn.addEventListener('click', function(){
     // * creiamo un ciclo For per stampare i numeri
     for (let i = 0; i <= 100; i++){
 
+        let number = i;
         // * stampare i numeri da 1 a 100
         // console.log(i);
 
         // * condizione per verificare se i è multiplo di 3 e se i è multiplo di 5
-        if ( (i % 3 == 0) && (i % 5 == 0) ) { // ! se i diviso 3 dà resto 0 è multiplo di 3 e se i diviso 5 dà resto 0 è multiplo di 5
+        if ( (number % 3 == 0) && (number % 5 == 0) ) { // ! se i diviso 3 dà resto 0 è multiplo di 3 e se i diviso 5 dà resto 0 è multiplo di 5
 
-            console.log(i + ' FizzBuzz' + ' questo è un multiplo di 3 e 5!');
+            console.log('FizzBuzz ' + number);
+
+            // ! cambio il value del number in FizzBuzz
+            number = 'FizzBuzz';
 
             // ? ho creato l'elemento tag LI per ogni numbero multipli di 3 e 5
             const newLi = document.createElement('li');
-            newLi.innerText = i;
+            newLi.innerText = `${i} ${number}`;
 
             document.getElementById('my-list').append(newLi);
             
 
-        } else if (i % 3 == 0) { // ! se i diviso 3 dà resto 0 è multiplo di 3
+        } else if (number % 3 == 0) { // ! se i diviso 3 dà resto 0 è multiplo di 3
 
-            console.log(i + ' Fizz' + ' questo è un multiplo di 3');
+            console.log('Fizz ' + number);
 
-        } else if (i % 5 == 0) { // ! se i diviso 5 dà resto 0 è multiplo di 5
+            // ! cambio il value del number in Fizz
+            number = 'Fizz';
 
-            console.log(i + ' Buzz' + ' questo è un multiplo di 5');
+            // ? ho creato l'elemento tag LI per ogni numbero multipli di 3 
+            const newLi = document.createElement('li');
+            newLi.innerText = `${i} ${number}`;
+
+            document.getElementById('my-list').append(newLi);
+
+        } else if (number % 5 == 0) { // ! se i diviso 5 dà resto 0 è multiplo di 5
+
+            console.log('Buzz ' + number);
+
+            // ! cambio il value del number in Buzz
+            number = 'Buzz';
+
+            // ? ho creato l'elemento tag LI per ogni numbero multipli di 5
+            const newLi = document.createElement('li');
+            newLi.innerText = `${i} ${number}`;
+
+            document.getElementById('my-list').append(newLi);
 
         } else {
 
-            console.log(i + ' questo NON è multiplo di 3 e 5');
+            console.log(number);
+
+            // ? ho creato l'elemento tag LI per ogni numero
+            const newLi = document.createElement('li');
+            newLi.innerText = number;
+
+            document.getElementById('my-list').append(newLi);
 
         }
 
