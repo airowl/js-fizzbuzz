44715c03e5fc5b1f55ac2fc44db5831efeafc71a
rowl <airowl.gasga@gmail.com>
2022-02-24 15:40:11 +0100

    inserito ciclo for per stampare i numbers e inserito condizione per il calcolo dei multipli

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 
 /* 
 
-TODO:   1) Stampare un consolo log "per adesso" i numeri da 1 a 100 
+TODO:   1) Stampare un consolo log "per adesso" i numeri da 1 a 100 OK
 TODO:   2) fare un event listener con un bottone OK
 TODO:   3) Creare un ciclo FOR OK
 TODO:   4) Creare un condizione IF 
@@ -9,6 +9,7 @@ TODO:       -   se il numero x è multiplo di 3 (numbers % 3 == 0)
 TODO:       -   fai questo (numbers = "Fizz") // solo se numbers è multiplo di 3
 TODO:       -   altrimenti se x è multiplo di 5 (numbers % 5 == 0)
 TODO:       -   fai questo (numbers = "Buzz") // solo se numbers è multiplo di 5
+TODO:       -   altrimenti (numbers = "FizzBuzz")
 
 */
 
@@ -18,10 +19,19 @@ const btn = document.getElementById('my-btn');
 btn.addEventListener('click', function(){
 
     // * creiamo un ciclo For per stampare i numeri
-    for (let i = 0; i < 100; i++){
+    for (let i = 0; i <= 100; i++){
 
     // * stampare i numeri da 1 a 100
-    console.log(i);
+    // console.log(i);
+
+    // * condizione per verificare se i è multiplo di 3 e se i è multiplo di 5
+    if (i % 3 == 0) {
+        console.log(i + ' Fizz' + ' questo è un multiplo di 3');
+    } else if (i % 5 == 0) {
+        console.log(i + ' Buzz' + ' questo è un multiplo di 5');
+    } else{
+        console.log(i + ' FizzBuzz' + ' questo non è multiplo di 3 e 5');
+    }
 
     }
 });
