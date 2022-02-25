4577c7ab815aba862c7bdbcd0e7749735a9a339d
rowl <airowl.gasga@gmail.com>
2022-02-25 09:01:05 +0100

    minori modifiche

--- js/script.js
+++ js/script.js
@@ -1,18 +1,4 @@
 
-/* 
-
-TODO:   1) Stampare un consolo log "per adesso" i numeri da 1 a 100 OK
-TODO:   2) fare un event listener con un bottone OK
-TODO:   3) Creare un ciclo FOR OK
-TODO:   4) Creare un condizione IF 
-TODO:       -   se il numero x è multiplo di 3 (numbers % 3 == 0)
-TODO:       -   fai questo (numbers = "Fizz") // solo se numbers è multiplo di 3
-TODO:       -   altrimenti se x è multiplo di 5 (numbers % 5 == 0)
-TODO:       -   fai questo (numbers = "Buzz") // solo se numbers è multiplo di 5
-TODO:       -   altrimenti (numbers = "FizzBuzz")
-
-*/
-
 // * creiamo un bottone per eseguire l'event listener
 const btn = document.getElementById('my-btn');
 
